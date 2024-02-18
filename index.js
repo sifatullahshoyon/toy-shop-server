06412a351d627dfe7edb0bbafde8381dd605a0c4
freelancersifat627e <freelancersifat627@gmail.comm>
2024-02-18 15:36:41 +0600

    u

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const galleryCollection = client.db("toyDB").collection("toyGallery");
     const productCollection = client.db('toyDB').collection('toyProducts');
